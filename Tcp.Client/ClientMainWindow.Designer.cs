fe9fed0b792373263121b13a515621e069907ac5
Grand-Admiral Sanya <38395460+AlexandrGulyaev@users.noreply.github.com>
2020-06-14 14:45:12 +0300

    Add files via upload

---
+++ Tcp.Client/ClientMainWindow.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace SomeProject.TcpClient
+{
+    partial class ClientMainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.sendMsgBtn = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SendFileBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // sendMsgBtn
+            // 
+            this.sendMsgBtn.Location = new System.Drawing.Point(263, 216);
+            this.sendMsgBtn.Name = "sendMsgBtn";
+            this.sendMsgBtn.Size = new System.Drawing.Size(147, 23);
+            this.sendMsgBtn.TabIndex = 0;
+            this.sendMsgBtn.Text = "Send Message";
+            this.sendMsgBtn.UseVisualStyleBackColor = true;
+            this.sendMsgBtn.Click += new System.EventHandler(this.OnMsgBtnClick);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(37, 12);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox.Size = new System.Drawing.Size(373, 193);
+            this.textBox.TabIndex = 1;
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.labelRes.Location = new System.Drawing.Point(37, 212);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(0, 17);
+            this.labelRes.TabIndex = 2;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.OnTimerTick);
+            // 
+            // SendFileBtn
+            // 
+            this.SendFileBtn.Location = new System.Drawing.Point(110, 216);
+            this.SendFileBtn.Name = "SendFileBtn";
+            this.SendFileBtn.Size = new System.Drawing.Size(147, 23);
+            this.SendFileBtn.TabIndex = 0;
+            this.SendFileBtn.Text = "Send File";
+            this.SendFileBtn.UseVisualStyleBackColor = true;
+            this.SendFileBtn.Click += new System.EventHandler(this.OnFileBtnClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ClientMainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 260);
+            this.Controls.Add(this.labelRes);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.SendFileBtn);
+            this.Controls.Add(this.sendMsgBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ClientMainWindow";
+            this.Text = "Client application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sendMsgBtn;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Label labelRes;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button SendFileBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Tcp.Client/ClientMainWindow.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Windows.Forms;
+using SomeProject.Library.Client;
+using SomeProject.Library;
+
+namespace SomeProject.TcpClient
+{
+    public partial class ClientMainWindow : Form
+    {
+        /// <summary>
+        /// Инициализация окна клиента
+        /// </summary>
+        public ClientMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обработка нажатия на кнопку "Отправить сообщение"
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void OnMsgBtnClick(object sender, EventArgs e)
+        {
+            Client client = new Client();
+            Result res = client.SendMessageToServer(textBox.Text).Result;
+            if(res == Result.OK)
+            {
+                textBox.Text = "";
+                labelRes.Text = "Message was sent succefully!";
+            }
+            else
+            {
+                labelRes.Text = "Cannot send the message to the server.";
+            }
+            timer.Interval = 2000;
+            timer.Start();
+        }
+        /// <summary>
+        /// Очистка по истечении времени
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            labelRes.Text = "";
+            timer.Stop();
+        }
+
+        /// <summary>
+        /// Обработка нажатия на кнопку "Отправить файл"
+        /// </summary>
+        /// <param name="sender">sender</param>
+        /// <param name="e">EventArgs</param>
+        private void OnFileBtnClick(object sender, EventArgs e)
+        {
+            Client client = new Client();
+            OperationResult res;
+            string path = SelectFile();
+
+            if (path != null) res = client.SendFileToServer(path);
+            else return;
+
+            labelRes.Text = res.Message;
+
+            timer.Interval = 2000;
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Выбор файла
+        /// </summary>
+        /// <returns>Путь к файлу</returns>
+        private string SelectFile()
+        {
+            using (OpenFileDialog OFileDialog = new OpenFileDialog())
+            {
+                if (OFileDialog.ShowDialog(this) == DialogResult.OK)
+                {
+                    return OFileDialog.FileName;
+                }
+                else
+                {
+                    labelRes.Text = "Неверный путь к файлу";
+                    timer.Interval = 2000;
+                    timer.Start();
+                    return null;
+                }
+            }
+        }
+    }
+}
